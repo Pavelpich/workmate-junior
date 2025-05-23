69e0fb0e056c892d94f0d4e65d64c7b633b9e63c
pavel <pavelpichugin28@gmail.com>
2025-05-23 17:20:32 +0200

    formulated project structure

---
+++ employee_salary_report/main.py
@@ -0,0 +1,7 @@
+
+def driver_func():
+    pass
+
+
+if __name__ == "__main__":
+    driver_func()
--- solution.py
+++
@@ -1,2 +0,0 @@
-# https://docs.google.com/document/d/1Kyj6X7poy2e1lKUaZasNDIFnMYdAlyDdQ9FCBkBCe1s/edit?tab=t.0#heading=h.or54d8e34zbk
-
