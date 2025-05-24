f926c54fcd70b2ba9d492f0e6f8ac08030491e2a
pavel <pavelpichugin28@gmail.com>
2025-05-24 18:20:21 +0200

    developed csv_parser module more and added tests

--- employee_salary_report/parsers/csv_parser.py
+++ employee_salary_report/parsers/csv_parser.py
@@ -1,16 +1,52 @@
+from os.path import exists
+from typing import Any
+
+
 class CSVParser:
+    columns_mapping = {
+        'salary':'hourly_rate',
+        'rate':'hourly_rate',
+        'department':'department',
+        'id':'id',
+        'email':'email',
+        'name':'name',
+        'hour_worked':'hours_worked'
+    }
+
     def __init__(self, file_path: str):
+        if not exists(file_path):
+            raise FileNotFoundError(f"Передан некорректный путь к файлу: '{file_path}'")
+
+        if file_path.split(".")[-1] != "csv":
+            raise TypeError(
+                f"Файл {file_path} переданный в парсер не является файлом .csv"
+            )
+
         self.file_path = file_path
         self.headers = []
 
-    def parse(self) -> list[dict[str, str]]:
-        with open(self.file_path, 'r', encoding='utf-8') as f:
+    @staticmethod
+    def _cast_val(value: Any) -> str | int | float:
+        value = value.strip()
+        if value.isdigit():
+            return int(value)
+
+        try:
+            return float(value)
+        except ValueError:
+            return value
+
+    def parse(self) -> list[dict[str, str | int | float]]:
+        with open(self.file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
 
-        self.headers = lines[0].strip().split(',')
+        parsed_headers = lines[0].strip().split(",")
+        self.headers = [self.columns_mapping.get(it, it) for it in parsed_headers]
+
         records = []
         for line in lines[1:]:
-            values = line.strip().split(',')
+            splitted_line = line.strip().split(",")
+            values = [self._cast_val(it) for it in splitted_line]
             record = dict(zip(self.headers, values))
             records.append(record)
         return records
--- employee_salary_report/tests/test_csv_reader.py
+++ employee_salary_report/tests/test_csv_reader.py
@@ -1,16 +1,48 @@
 import pytest
 from employee_salary_report.parsers.csv_parser import CSVParser
 
-def test_employee_creation_valid():
-    # Given & When
-    parsed_data = CSVParser("./csv_reports/data1.csv")
+def test_parse_given_empty_path_raises_file_not_found():
+    # Given & When & Then
+    with pytest.raises(FileNotFoundError):
+        CSVParser("")
+
+def test_parse_given_invalid_path_raises_file_not_found():
+    # Given & When & Then
+    with pytest.raises(FileNotFoundError):
+        CSVParser("./csv_reports/data_sata12.csv")
+
+def test_parse_given_non_csv_extension_raises_error():
+    # Given & When & Then
+    with pytest.raises(TypeError):
+        CSVParser("./csv_reports/totaly_csv.txt")
+
+def test_parser_work_returns_valid_outcome():
+    # Given
+    parser_obj = CSVParser("./csv_reports/data1.csv")
+
+    # When
+    res = parser_obj.parse()
 
     # Then
-    assert parsed_data["email"] == email
-    assert parsed_data["name"] == name
-    assert parsed_data["department"] == department
-    assert parsed_data["hours_worked"] == hours_worked
-    assert parsed_data["id"] == id
-    assert parsed_data["hourly_rate"] == hourly_rate
+    assert res[0]["email"] == "alice@example.com"
+    assert res[0]["name"] == "Alice Johnson"
+    assert res[0]["department"] == "Marketing"
+    assert res[0]["hours_worked"] == 160
+    assert res[0]["id"] == 1
+    assert res[0]["hourly_rate"] == 50
 
+    assert res[2]["email"] == "carol@example.com"
+    assert res[2]["name"] == "Carol Williams"
+    assert res[2]["department"] == "Design"
+    assert res[2]["hours_worked"] == 170
+    assert res[2]["id"] == 3
+    assert res[2]["hourly_rate"] == 60
 
+def test_csvparser_work_validity():
+    # Given
+    parser_obj_1 = CSVParser("./csv_reports/data1.csv")
+    parser_obj_2 = CSVParser("./csv_reports/data2.csv")
+    parser_obj_3 = CSVParser("./csv_reports/data3.csv")
+
+    assert parser_obj_1.headers == parser_obj_2.headers
+    assert parser_obj_2.headers == parser_obj_3.headers
