8cf1df0d53c95615c6f70c065c358ec8b60787b3
pavel <pavelpichugin28@gmail.com>
2025-05-25 02:19:27 +0200

    added tests for output

---
+++ employee_salary_report/tests/test_output.py
@@ -0,0 +1,16 @@
+import pytest
+from employee_salary_report.output.console_output import  ConsoleOutput
+from employee_salary_report.reports.payout_report import PayoutReport
+from employee_salary_report.output.json_output import JsonOutput
+from models.employee import Employee
+
+def test_console_output_renders_correctly():
+
+
+def test_console_output_with_empty_data():
+
+
+def test_json_output_renders_correctly():
+
+
+def test_json_output_with_empty_data():
