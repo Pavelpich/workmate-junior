e3bde73223c8e403f0c8dbf2b0ae9d60a033c024
pavel <pavelpichugin28@gmail.com>
2025-05-25 04:42:11 +0200

    to prev

--- employee_salary_report/parsers/csv_parser.py
+++ employee_salary_report/parsers/csv_parser.py
@@ -4,13 +4,13 @@ from typing import Any
 
 class CSVParser:
     columns_mapping = {
-        'salary':'hourly_rate',
-        'rate':'hourly_rate',
-        'department':'department',
-        'id':'id',
-        'email':'email',
-        'name':'name',
-        'hour_worked':'hours_worked'
+        "salary": "hourly_rate",
+        "rate": "hourly_rate",
+        "department": "department",
+        "id": "id",
+        "email": "email",
+        "name": "name",
+        "hour_worked": "hours_worked",
     }
 
     def __init__(self, file_path: str):
@@ -36,12 +36,21 @@ class CSVParser:
             try:
                 return float(value)
             except ValueError:
-                raise ValueError(f"Передано неверное значение для поля {field_name}: {value}")
+                raise ValueError(
+                    f"Передано неверное значение для поля {field_name}: {value}"
+                )
         else:
             return value
 
     def parse(self) -> list[dict[str, str | int | float]]:
-        required_columns = {"id", "email", "name", "department", "hours_worked", "hourly_rate"}
+        required_columns = {
+            "id",
+            "email",
+            "name",
+            "department",
+            "hours_worked",
+            "hourly_rate",
+        }
         with open(self.file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
 
@@ -51,14 +60,21 @@ class CSVParser:
         # Проверяем, что все обязательные колонки есть в header
         if not required_columns.issubset(self.headers):
             missing = required_columns - set(self.headers)
-            raise ValueError(f"В файле {self.file_path} не хватает обязательных колонок в header: {missing}")
+            raise ValueError(
+                f"В файле {self.file_path} не хватает обязательных колонок в header: {missing}"
+            )
 
         records = []
         for line in lines[1:]:
             split_values = line.strip().split(",")
             if len(split_values) != len(self.headers):
-                raise ValueError(f"Строка содержит {len(split_values)} колонок, ожидалось {len(self.headers)}")
-            values = [self._cast_val(val, field) for val, field in zip(split_values, self.headers)]
+                raise ValueError(
+                    f"Строка содержит {len(split_values)} колонок, ожидалось {len(self.headers)}"
+                )
+            values = [
+                self._cast_val(val, field)
+                for val, field in zip(split_values, self.headers)
+            ]
             record = dict(zip(self.headers, values))
             records.append(record)
         return records
